d04743448a1e2ed65bc6b44dce024f36e5f7d88b
Karlos78 <karlmolden@hotmail.com>
2017-03-29 15:25:22 +0100

    Vignette written - struggling to get functions to work in knitted document

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -4,6 +4,7 @@ library(farsassignment)
 context("farstests")
 
 test_that("File name created", expect_match(make_filename(2016), "accident_2016.csv.bz2"))
+test_that("Non-integer value", expect_warning(make_filename("a")))
 
 
 
